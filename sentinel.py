92eeb0faf84b5c167608e02865f08bd75073d95f
Alex Nisnevich <alex.nisnevich@gmail.com>
2013-02-18 02:59:01 -0800

    removing installation instructions from sentinel.py, adding usage info

--- sentinel.py
+++ sentinel.py
@@ -4,30 +4,22 @@
 # A USB rocket launcher face-tracking solution
 # For Linux and Windows
 #
-# Hardware requirements:
-# - Dream Cheeky brand USB rocket launcher (tested with Thunder model, should also work with Storm)
-# - small webcam attached to USB rocket launcher, in /dev/video0
+# Installation: see README.md
 #
-# Software requirements (Linux):
-# - Python 2.7, 32-bit
-# - libusb (in Ubuntu/Debian, apt-get install libusb-dev)
-# - PyUSB 1.0 (https://github.com/walac/pyusb)
-# - NumPy (in Ubuntu/Debian, apt-get install python-numpy)
-# - OpenCV Python bindings (ihttp://jayrambhia.wordpress.com/2012/06/20/install-opencv-2-4-in-ubuntu-12-04-precise-pangolin/)
-# - PIL (in Ubuntu/Debian, apt-get install python-imaging)
-# - streamer (in Ubuntu/Debian, apt-get install streamer)
+# Usage: sentinel.py [options]
 #
-# Software requirements (Windows):
-# - Python 2.7, 32-bit
-# - libusb (http://sourceforge.net/projects/libusb-win32/files/)
-#     - After installing, plug in USB rocket launcher, launch <libusb path>\bin\inf-wizard.exe,
-#       and create and run an INF driver file for the USB rocket launcher using the wizard
-# - PyUSB 1.0 (https://github.com/walac/pyusb)
-# - NumPy (http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy)
-# - OpenCV Python bindings (http://sourceforge.net/projects/opencvlibrary/files/opencv-win/2.3.1/OpenCV-2.3.1-win-superpack.exe/download)
-#     - After installing, copy the contents of <opencv path>\build\python\2.7 (it should contain cv.py and cv2.pyd)
-#       to c:\Python27\Lib
-# - PIL (http://www.lfd.uci.edu/~gohlke/pythonlibs/#pil)
+# Options:
+#   -h, --help            show this help message and exit
+#   -a, --arm             enable the rocket launcher to fire
+#   -b SIZE, --buffer=SIZE
+#                         size of camera buffer. Default: 2
+#   -c NUM, --camera=NUM  specify the camera # to use. Default: 0
+#   -d WIDTHxHEIGHT, --dimensions=WIDTHxHEIGHT
+#                         image dimensions (recommended: 320x240 or 640x480).
+#                         Default: 320x240
+#   --nd, --no-display    do not display captured images
+#   -r, --reset           reset the camera and exit
+#   -v, --verbose         output timing information
 
 import os
 import sys
